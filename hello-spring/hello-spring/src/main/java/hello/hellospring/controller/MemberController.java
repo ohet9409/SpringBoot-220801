a4800ca07024e52aeacea620a7b297fa259a1c5f
oet <54170438+ohet9409@users.noreply.github.com>
2022-08-15 20:33:10 +0900

    회원 웹 기능 - 조회

--- hello-spring/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -47,4 +50,12 @@ public class MemberController {
         return "redirect:/";
     }
 
+    //  조회 화면
+    @GetMapping("/members")
+    public String List(Model model) {
+        List<Member> members = memberService.findMembers();
+        // 화면으로 전달될 데이터 key, value
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
